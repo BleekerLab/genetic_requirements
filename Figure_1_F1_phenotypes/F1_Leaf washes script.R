f4097980af58d2cf883729416e3b210414649119
Ruy Kortbeek <R.W.J.kortbeek@uva.nl>
2019-09-20 15:05:44 +0200

    added PCA

--- Figure_1_F1_phenotypes/F1_Leaf washes script.R
+++ Figure_1_F1_phenotypes/F1_Leaf washes script.R
@@ -1,67 +1,46 @@
+library(tidyverse)
+library(ggbiplot)
+
+
 #Create_dataframe
-df <- read.delim("20170425_F1s leaf washes_peak area.txt", header = T, sep = "\t", dec = ".")
-str(df)
+df <- read.delim("Figure_1_F1_phenotypes/20170425_F1s leaf washes_peak area.txt", header = T, sep = "\t", dec = ".")
+df$total_terpenes = rowSums(df[,3:ncol(df)]) #make a new column and calculate total terpene ion counts
 
-#Change to long ('tidy') format
-df_long <- gather(df, metabolite, abundance, Bpinene:Abergamotoicacid, -genotype,-plant,factor_key=TRUE)
 
-attach(df_long)
+#Change to long ('tidy') format -> filter unwanted data (e.g. the LA1777-F1 measurements)
+df_long <- gather(df, metabolite, abundance, Bpinene:total_terpenes, -genotype,-genotype,factor_key=TRUE) %>% 
+  filter(., genotype != "CV_LA1777") %>% filter(., genotype != "F1-LA1777")
+
+
 
 #Define variables 
-df_long$plant <- factor(df_long$plant, levels = c("CV", "PI127826", "LA1777", "CVxPI127826", "CVxLA1777", "PI127826xLA1777", 
-                              ordered = TRUE))
-b <- factor(metabolite)
-c <- abundance
-df_zingiberene <- filter(df_long, 
-                         metabolite == "X7epizingiberene" & df_long$plant %in% c("PI127826", "CV", "CVxPI127826","PI127826xLA1777", ordered=TRUE))
+df_long$genotype <- factor(df_long$genotype, levels = c("CV", "PI127826", "F1", "F1-hab", "LA1777"),
+                              ordered = TRUE)
 
-df_zingiberene$plant <- factor(df_zingiberene$plant, levels =c("PI127826", "CV", "CVxPI127826","PI127826xLA1777",
-                               odered = TRUE))
-#Simple boxplot
-ggplot(data = df_zingiberene, aes(x = df_zingiberene$plant, y = df_zingiberene$abundance, fill=df_zingiberene$plant)) +
-  geom_boxplot() + 
-
-#For colour plot: 
-  scale_fill_brewer(palette="Dark2") +
-#For bw plot: 
-  #scale_fill_grey()+
-
-    geom_point() +
-     theme_classic() +
-      theme_linedraw() +
-        #facet_wrap(b, scales = 'free') +
-          theme(text = element_text(family = "Times New Roman", color = "black", size = 10),
-                axis.text.x = element_text(size = 10, angle = 0), 
-                plot.title = element_text(size = rel(2),hjust = 0.5),
-                strip.background = element_rect(colour="black", fill="gray"),
-                strip.text.x = element_text(size=8, colour = "black"),
-                panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank()) +
-  labs(title = "7-epizingiberene in extracts", 
-       x = "Genotype",
-       y = "7-epizingiberene (ion counts / mg fresh leaf material)") +
-ggsave("F1_leafwashes_zinigberene_boxplot.svg", device = "svg", scale = 1, width = 10, height = 3, units = "cm", dpi = 300)
+#summarise data for barplot
+sum = summarySE(df_long, measurevar = "abundance", groupvars = c("genotype", "metabolite"))
 
 #### BARPLOT ###
 
-zing_summary <- summarySE(df_zingiberene, measurevar="abundance", groupvars=c("plant", "metabolite"))
-
-ggplot(data = zing_summary, aes(x = zing_summary$plant, y = zing_summary$abundance, fill=zing_summary$plant)) +
+p.leafwash =
+sum %>% filter(., metabolite %in% c("total_terpenes", "X7epizingiberene")) %>%
+ggplot(., aes(x = genotype, y = abundance)) +
   geom_bar(stat = "identity", color = "black", fill = "black") +
-    geom_errorbar(aes(ymin=abundance-se, ymax=abundance+se), width=.2) +
-  scale_fill_brewer(palette="Dark2") +
-  theme_classic() +
-  theme_linedraw() +
-  theme(text = element_text(family = "Times New Roman", color = "black", size = 6),
-        axis.text.x = element_text(size = 6, angle = 0),
-        axis.text.y = element_text(size = 6, angle = 0),
-        plot.title = element_text(size = rel(2),hjust = 0.5),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())+
-  labs(title = "7-epizingiberene in extracts", 
-       x = "Genotype",
-       y = "7-epizingiberene (ion counts / mg fresh leaf material)") +
-  ggsave("F1_leafwashes_zinigberene_barplot.svg", device = "svg", scale = 1, width = 10, height = 6, units = "cm", dpi = 300)
+    geom_errorbar(aes(x = genotype, ymin=abundance-se, ymax=abundance+se), width=.2) +
+    my_theme +
+  facet_grid(~metabolite)+
+  ylab("Metabolite abundance (ion counts / mg fresh leaf)")+
+  xlab(NULL)
+
+ggsave("Figure_1_F1_phenotypes/plots/F1_leafwashes_barplot.pdf", device = "pdf", scale = 1, width = 10, height = 6)
+
+#check for normality
+shapiro.test(df$total_terpenes) 
+qqPlot(df$X7epizingiberene)
+
+kruskal.test(data = df, total_terpenes ~  genotype)
+leafwash.wilcox = wilcox.test(df$genotype, df$total_terpenes)
+capture.output(leafwash.wilcox, file = "Figure_1_F1_phenotypes/plots/leafwashes_wilcox.txt")
   
 
 #ANOVA
@@ -72,3 +51,36 @@ summary(aov_genotype)
 TK <- TukeyHSD(aov_genotype, "genotype", ordered = TRUE)
 TukeyHSD_results = as.data.frame(TK$genotype)
 write.csv(TukeyHSD_results, file = "F1_leafwash_TukeyHSD_results.csv")
+
+############
+#   PCA    #
+############
+
+df2 = df %>% column_to_rownames(., var = "sample") %>% select(., -total_terpenes) %>%
+  filter(., genotype != "CV_LA1777") %>% filter(., genotype != "F1-LA1777")
+
+genotypes= df2[,1]
+df2 = df2[,2:ncol(df2)]+1
+df.log = log(df2)
+
+df.pca = prcomp(df.log)
+summary(df.pca)
+
+# Plot PCS
+
+my.theme = 
+  theme(text = element_text(),
+        axis.text.x = element_text(size = 8, angle = 0, colour = "black"), 
+        strip.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(),
+        panel.background = element_rect(fill = NA, color = "black"),
+        strip.text.x = element_text(size=8, colour = "black")
+  )+
+  theme_bw()
+
+p.pca = 
+ggbiplot(df.pca, ellipse = TRUE, groups = genotypes, var.axes = FALSE)+my.theme
+
+ggsave(filename = "Figure_1_F1_phenotypes/plots/PCA_F1_leafwash.pdf", plot = p.pca, width = 12, height = 16, units = "cm")
