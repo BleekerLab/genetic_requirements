784f055fc68895c2a314b6facd5ad9de0b062ba7
Ruy Kortbeek <R.W.J.kortbeek@uva.nl>
2019-10-08 14:20:25 +0200

    added scatterplots

--- Figure_S3/Barplot_Full F2.R
+++ Figure_S3/Barplot_Full F2.R
@@ -6,7 +6,7 @@ library(tidyverse)
 
 my.theme = 
   theme(text = element_text(),
-        axis.text.x = element_text(size = 1, colour = "black"),
+        axis.text.x = element_text(size = 8, colour = "black"),
         axis.text.y = element_text(size = 8, colour = "black"), 
         strip.background = element_blank(),
         panel.grid.major = element_blank(),
@@ -48,5 +48,58 @@ ggplot(volatiles.long)+
   my.theme +
   ylab("Summed volatiles (ion counts / leaflet)")+
   xlab("F2 progeny")
-  
-ggsave("Figure_S3/Barplot_volatiles_Full_F2.svg", plot = p.bar, height = 4, width = 8)
+
+# Save plot
+ggsave("Figure_S3/Barplot_volatiles_Full_F2.svg", plot = p.bar, height = 4, width = 10)
+
+#################
+# Scatter plots #
+#################
+
+#Create linear model
+model  = lm(data = volatiles, log(caryophyllene+1) ~ log(zingiberene+1))
+summary(model)
+
+#Plot
+p.caryophyllene = 
+ggplot(volatiles, aes(x = log(zingiberene+1), y = log(caryophyllene+1)))+
+geom_point()+
+  geom_smooth(method = "lm", formula = y~x)+
+  annotate(geom = "text", x = 14.7, y = 10, 
+           label = round(summary(model)$adj.r.squared, digits= 3), 
+           size = 2) + #this plots the R-squared into the plot
+  annotate(geom = "text", x = 14.2, y = 10, 
+           label = "r^2 =",
+           size = 2)+
+my.theme+
+  xlab("7-epizingiberene (Log10-scaled ion counts / leaflet)")+
+  ylab("2-Caryophyllene (Log10-scaled ion counts / leaflet)")
+
+
+#Create model for pinene vs zingiberene
+model2  = lm(data = volatiles, log(pinene+1) ~ log(zingiberene+1))
+summary(model2)
+
+#Plot
+p.pinene = 
+  ggplot(volatiles, aes(x = log(zingiberene+1), y = log(pinene+1)))+
+  geom_point()+
+  geom_smooth(method = "lm", formula = y~x)+
+  annotate(geom = "text", x = 14.7, y = 6, 
+           label = round(summary(model2)$adj.r.squared, digits= 3), 
+           size = 2) +
+  annotate(geom = "text", x = 14.2, y = 6, 
+           label = "r^2 =",
+           size = 2)+
+  my.theme+
+  xlab("7-epizingiberene (Log10-scaled ion counts / leaflet)")+
+  ylab("a-pinene (Log10-scaled ion counts / leaflet)")
+
+##############
+# Save plots #
+##############
+
+ggsave("Figure_S3/zingiberene_vs_caryophyllene.svg", plot = p.caryophyllene, height = 4, width = 4)
+ggsave("Figure_S3/zingiberene_vs_pinene.svg", plot = p.pinene, height = 4, width = 4)
+
+
