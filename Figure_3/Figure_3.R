0f38db3801ec9ebbf21cd076fe8ae6e43ac13bb9
Marc Galland <m.galland@uva.nl>
2020-02-27 16:18:27 +0100

    update version of Figure 3A

--- Figure_3/Figure_3.R
+++ Figure_3/Figure_3.R
@@ -3,13 +3,18 @@ if (! "checkpoint" %in% installed.packages()){
 }
 
 library("checkpoint")
-checkpoint("2020-01-01")
+#checkpoint("2020-01-01")
+
+# nicely formatted table summaries
+library(gtsummary)
+library(gt)
 
 library(tidyverse)
 library(RColorBrewer)
 library(multcompView)
 library(Rmisc)
 library(FSA)
+
 #############################
 # Custom theme for plotting #
 #############################
@@ -19,43 +24,19 @@ source("theme.R")
 # Load data #
 #############
 
-trichomes = read.csv(file= "Figure_2/type_VI_trichomes_full_F2.csv", 
-                     header = T, stringsAsFactors = T)
-volatiles = read.csv(file = "Figure_2/GCMS_Leafwash_Full_F2_EZ_clean_data.csv", 
-                     header = T, stringsAsFactors = T, check.names = F)
-homo = read.csv(file = "Figure_2/homozygous_ZGB_genotypes_F2.csv", 
-                header = T, stringsAsFactors = T) #List with plants homozygous for zFPS/ZIS
-volatiles_VI = read.csv(file = "Figure_2/type_VI_gland_terpenes_F2.csv", 
-                        header = T, stringsAsFactors = T, check.names = F)
-
-# Take only the volatile data from plants homozygous for zFPS/ZIS + P450
-volatiles.homo = left_join(homo, volatiles, by = "genotype")
-
-# Remove rows with NA values
-volatiles.homo = volatiles.homo[complete.cases(volatiles.homo), ] 
-
-# subset parents from original data
-volatiles.parents = volatiles %>% filter(., group != "F2")        
-
-# join homozygous F2 plants with parents
-volatiles = full_join(volatiles.homo, volatiles.parents)          
-
-
-# Merge datasets of volatiles with trichome counting (/estimating)
-df = inner_join(volatiles, trichomes)
-df$genotype = as.factor(df$genotype)
-
+df = read.delim("Figure_3/volatiles_and_trichomes.tsv", 
+                header = T, 
+                stringsAsFactors = F)
 
 ################################
 # Sum trichome counts #
 ################################
 
 df2 = df %>% 
-  dplyr::group_by(genotype) %>% 
+  dplyr::group_by(genotype, zingiberene, type_VI) %>% 
   # take the sum of abaxial + adaxial surface (leaf wash)
   dplyr::summarise(., sum_type_VI = sum(type_VI)) %>% 
-  left_join(., volatiles, by = "genotype") %>%       # re-add the volatiles info
-  filter(sum_type_VI <= 10) %>%                      # maximum class
+  filter(sum_type_VI <= 10) %>%                      # maximum class value
   mutate(sum_type_VI = as.factor(sum_type_VI))       # convert int to factor
  
 # to create C2 to C10 classes of trichome counts
@@ -63,89 +44,115 @@ levels(df2$sum_type_VI) = paste("C",
                                levels(df2$sum_type_VI),
                                sep = "") 
 
+#####################################################
+# Figure 3A: boxplot type-VI density vs zingiberene #
+#####################################################
 
+# statistics
+# log2(zingiberene + 1) follows a normal distribution so ANOVA can be used
 
-##########################################
-# Boxplot type-VI density vs zingiberene #
-##########################################
+# ANOVA and Tukey HSD
+aov_res <- aov(formula = log2(zingiberene + 1) ~ 1 + sum_type_VI, data = df2)
+hsd_res <- TukeyHSD(aov_res, which = "sum_type_VI")
+groups <- as.data.frame(multcompLetters(hsd_res$sum_type_VI[,4])$Letters)
+colnames(groups) = "group"
+groups$sum_type_VI = row.names(groups)
+
+groups_trichome_class = pivot_longer(
+  data = groups,
+  cols = "sum_type_VI", 
+  names_to = "type", 
+  values_to = "class")
+
+# plot
+max_y_figure_3a = max(log2(df2$zingiberene + 1)) + 
+  0.1 * max(log2(df2$zingiberene + 1))  
 
 p.box = 
-ggplot(df2, aes(x = df2$sum_type_VI, y = df2$zingiberene)) +
-  geom_boxplot(aes(x = as.factor(df2$sum_type_VI), y = df2$zingiberene), 
-               fill = "grey", outlier.size = 0.5) +
-  geom_jitter(aes(x = as.factor(df2$sum_type_VI), y = df2$zingiberene),
-              size = 0.5, width = 0.2) +
-  #geom_smooth(method = "lm") +
-  ylim(NA, 2000000) +
-  xlab(NULL) +
+  df2 %>%
+#  filter(zingiberene > 0) %>% 
+  ggplot(aes(x = sum_type_VI, y = log2(zingiberene + 1))) +
+  geom_boxplot(fill = "grey", 
+               outlier.size = 0.5) +
+  geom_jitter(size = 0.5, 
+              width = 0.1, 
+              height = 0.1) +
   ylab("7-epizingiberene (ion counts / leaflet)") +
-  xlab("Type-VI trichome-density class") +
-  my_theme
+  xlab("Type-VI trichome density class") +
+  my_theme +
+  geom_text(data = groups_trichome_class, 
+            mapping = aes(x = class, y = max_y_figure_3a, label = group))
 
-ggsave(file = "Figure_3/plot_type-VI_class_vs_zingiberene.pdf", 
+
+p.box 
+
+
+ggsave(file = "Figure_3/Figure3A.pdf", 
        plot = p.box, 
-       width = 4, 
-       height = 4)
+       width = 7, 
+       height = 5)
 
-
-##############
-# Statistics #
-##############
-
-# Test for normality
-shapiro.test(df2$zingiberene)
-
-# Kruskal-Wallis test
-kruskal.test(data = df2, zingiberene~sum_type_VI)
-
-# Dunn's test for pairwise comparison
-dt = dunnTest(data = df2, zingiberene ~ sum_type_VI, method = "bh")
-
-# show signficant comparisons from Dunn's test
-sig.groups = dt$res[,c(1,4)] %>% filter(., P.adj < 0.05)
+ggsave(file = "Figure_3/Figure3A.png", 
+       plot = p.box, 
+       width = 7, 
+       height = 5)
 
 
 
-#########################################
-# Optional: Distribution of the classes #
-#########################################
+##################################################
+# Figure 3B: trichome densities versus zingiberene
+##################################################
 
-# Barplot
-sum = summarySE(df2, measurevar = "zingiberene", groupvars = "sum_type_VI")
-
-# p.density.class.zingiberene = 
-ggplot(sum, aes(x = sum_type_VI, y = zingiberene))+
-  geom_boxplot(aes(x = sum_type_VI, y = sum$zingiberene), 
-           stat= "identity", 
-           fill = "black") +
-  geom_point(data = df2, aes(x = df2$sum_type_VI, y = df2$zingiberene))+
-  geom_smooth(method = lm)+
-  geom_errorbar(aes(x = sum$sum_type_VI, ymin = sum$zingiberene - se, ymax = sum$zingiberene + se), width = 0.2)+
-  #scale_x_continuous(breaks=c(1:10))+
-  ylim(NA, 400000)+
-  ylab("7-epizingiberene (ion counts / leaflet)")+
-  xlab("trichome-density class")+
-  theme_bw() 
+df = read.csv("Figure_3/Leafwash vs Trichome density.csv",
+              header = TRUE, 
+              stringsAsFactors = T, 
+              check.names = FALSE)
 
 
-#############################################################################################
-# Supplemental ?                                                                            #
-# highlighting the selected lines for individual type-VI gland investigation                #
-#############################################################################################
-df.parsed  = na.omit(left_join(volatiles_VI, df2, by = "genotype"))
+# Change to long ('tidy') format. First the volatiles (df.long) and than also the trichomes (df.long2)
+df.long = gather(
+  data = df,
+  key = "metabolite",
+  value = level,
+  -sample, -group, -Type_VI_mm2_Abaxial,-Type_VI_mm2_Adaxial,-Type_VI_mm)
 
-p.box.highlight.subset = 
-  ggplot(df2, aes(x = df2$sum_type_VI, y = df2$zingiberene))+
-  geom_boxplot(aes(x = as.factor(df2$sum_type_VI), y = df2$zingiberene), fill = "grey", outlier.size = 0.5)+
-  geom_point(aes(x = as.factor(df2$sum_type_VI), y = df2$zingiberene),size = 0.5)+
-  geom_jitter(data = df.parsed, aes(x = na.omit(sum_type_VI)-1, y = zingiberene.y), color = "red", width = .05)+
-    ylim(0,500000)+
-  xlab(NULL)+
-  ylab("7-epizingiberene (ion counts / leaflet)")+
-  xlab("Type-VI trichome-density class")+
-  my_theme
+df.long2 = gather(
+  data = df.long,
+  key = "trichome_position",
+  value = density,
+  -sample,-metabolite,-group,-level)
 
-ggsave(file = "Figure_3/plot_type-VI_class_vs_zingiberene_highlight_subset.pdf", 
-       plot = p.box.highlight.subset, 
-       width = 4, 
-       height = 4)
+
+# Filter datafile on total_volatiles and type_VI_trichomes both abaxial and adaxial
+df.parsed = df.long2 %>% filter(., df.long2$metabolite == "total_volatiles" & df.long2$trichome_position == "Type_VI_mm") %>% filter(., !sample == "250")
+df.parsed$metabolite = as.factor(df.parsed$metabolite)
+df.parsed$trichome_position = as.factor(df.parsed$trichome_position)
+
+# linear model fitting the summed terpenes to the density of trichomes
+model  = lm(data = df.parsed, level ~ density)
+summary(model)
+
+# Scatterplot
+p.scatter = 
+  ggplot(df.parsed, aes(x=density, y=level)) +
+  geom_point(size = 0.5) +
+  geom_smooth(method = "lm", formula = y ~ x, alpha  = 0.2) + 
+  my_theme+
+  ylab("Summed terpenes (ng / mg fresh leaf)")+
+  xlab("Type-VI trichome density (trichomes / mm2)")+
+  geom_text(aes(label=df.parsed$sample),hjust=1.5, vjust=0.5, size = 1.5)+
+  annotate(geom = "text", x = 22, y = 320, 
+           label = round(summary(model)$adj.r.squared, digits= 3), 
+           size = 3) +
+  annotate(geom = "text", x = 19.5, y = 320, 
+           label = "r^2=",
+           size = 3)
+
+# Save plot
+ggsave(file = "Figure_3/trichome_density_VS_leafwash.pdf", plot = p.scatter, height = 5, width = 4)
+
+
+
+
+
+
---
+++ Figure_3/README.md
@@ -0,0 +1,10 @@
+# Figure 3
+
+## Figure 3 legend
+Some legend here  
+(A)   
+(B)   
+
+## Figure preview 
+
+![Figure 3A](./Figure3A.png "Figure 3A")
