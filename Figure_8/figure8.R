ba096c29f57fc8ea87f3b904a2d90c102880b4f3
Marc Galland <m.galland@uva.nl>
2020-03-24 18:04:56 +0100

    obtain melted dataframe of count percentages from its genome

--- Figure_8/figure8.R
+++ Figure_8/figure8.R
@@ -9,10 +9,10 @@ library(tidyverse)
 ###############
 
 # Counts based on the Heinz1706 genome 
-heinz <- read.delim("Figure_8/counts_on_Heinz1706.tsv", stringsAsFactors = F, check.names = F)
+heinz <- read.delim("Figure_8/counts_on_heinz.tsv", stringsAsFactors = F, check.names = F)
 
 # Counts based on the PI127826 genome
-habro <- read.delim("Figure_8/counts_on_PI12826.tsv", stringsAsFactors = F, check.names = F)
+habro <- read.delim("Figure_8/counts_on_habrochaites.tsv", stringsAsFactors = F, check.names = F)
 
 # remove columns not useful (Chr, Start, End, etc.)
 heinz <- heinz[,-c(2,3,4,5,6)] 
@@ -29,7 +29,9 @@ heinz <- heinz[heinz$target %in% habro$target,]
 habro <- habro %>% arrange(target) 
 
 # sanity check
-all_equal(habro$target, heinz$target)
+if (all_equal(habro$target, heinz$target) == FALSE){
+  stop()
+}
 
 ###################################
 # Conversion to matrix and addition
@@ -48,4 +50,24 @@ perc_heinz <- heinz / total_counts * 100
 # % of counts coming from habro (PI127826 genome)
 perc_habro <- habro / total_counts * 100 
 
+# add genes names
+perc_habro$target <- targets
+perc_heinz$target <- targets
 
+# add genome ref
+perc_habro$genome = "habrochaites"
+perc_heinz$genome = "lycopersicum"
+
+#############################################################
+# Creation of a combined dataframe with a "genome_ref" column
+# Goal = plot comparative barplots
+#############################################################
+
+final_df = bind_rows(perc_habro, perc_heinz) %>% 
+  pivot_longer(., 
+               cols = - c(target, genome),
+               names_to = "genotype", 
+               values_to = "counts") 
+
+  #ggplot(., aes(x = counts)) + 
+  #geom_density() 
