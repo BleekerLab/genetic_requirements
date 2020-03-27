b5a1deda0f8719d58143aca8870aac3883301c1e
Marc Galland <m.galland@uva.nl>
2020-03-27 17:12:51 +0100

    normalise with DESeq2

--- Figure_8/figure8.R
+++ Figure_8/figure8.R
@@ -2,7 +2,10 @@ library("checkpoint")
 
 checkpoint("2020-01-01")
 
-library(tidyverse)
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(DESeq2))
+
+source("Figure_8/R/normalise_counts.R")
 
 ###############
 # Import counts
@@ -18,34 +21,61 @@ habro <- read.delim("Figure_8/counts_on_PI12826.tsv", stringsAsFactors = F, chec
 heinz <- heinz[,-c(2,3,4,5,6)] 
 habro <- habro[,-c(2,3,4,5,6)] 
 
-###################################
+#######################
+# Normalise with DESeq2
+#######################
+
+row.names(heinz) <- heinz$target
+row.names(habro) <- habro$target
+
+cts_heinz <- heinz[,-1]
+cts_habro <- habro[,-1]
+
+col_data = data.frame(
+  sample = colnames(heinz)[2:ncol(heinz)], 
+  condition = c(rep("A",4), rep("B","5"))) # arbitrary conditions (not used)
+
+dds_heinz <- DESeqDataSetFromMatrix(countData = cts_heinz, colData = col_data, design = ~ condition)
+dds_habro <- DESeqDataSetFromMatrix(countData = cts_habro, colData = col_data, design = ~ condition)
+
+dds_heinz <- estimateSizeFactors(dds_heinz)
+dds_habro <- estimateSizeFactors(dds_habro)
+
+norm_heinz <- DESeq2::counts(dds_heinz, normalized = TRUE)
+norm_habro <- DESeq2::counts(dds_habro, normalized = TRUE)
+
+###################
 # Keep common genes
-###################################
+###################
 
 # keep only common target genes
-heinz <- heinz[heinz$target %in% habro$target,]
+norm_heinz <- norm_heinz[row.names(norm_heinz) %in% habro$target,]
 
 # reorder genes in the same order
-habro <- habro %>% arrange(target) 
+norm_habro <- norm_habro[order(row.names(norm_habro)),]
 
 # sanity check
-all_equal(habro$target, heinz$target)
+if (! all_equal(row.names(norm_habro), row.names(norm_heinz)) ) {
+  stop("problems with matching gene names")
+}
 
 ###################################
 # Conversion to matrix and addition
 ###################################
-# keep genes names
-targets <-heinz$target
 
-heinz <- heinz %>% select(- target) 
-habro <- habro %>% select(- target) 
-
-total_counts <- heinz + habro
+total_counts <- norm_heinz + norm_habro
 
 # % of counts coming from ELite (Heinz1706 genome)
-perc_heinz <- heinz / total_counts * 100 
+perc_heinz <- norm_heinz / total_counts * 100 
 
 # % of counts coming from habro (PI127826 genome)
-perc_habro <- habro / total_counts * 100 
+perc_habro <- norm_habro / total_counts * 100 
+
+
+# add genome ref
+perc_habro <- as.data.frame(perc_habro)
+perc_heinz <- as.data.frame(perc_heinz)
+perc_habro$genome = "habrochaites"
+perc_heinz$genome = "lycopersicum"
 
 
