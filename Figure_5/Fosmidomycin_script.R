de4805e319e2e2bf2957399738ba083168fbf9f2
Ruy Kortbeek <R.W.J.Kortbeek@uva.nl>
2020-04-22 20:48:16 +0200

    Deleted boxplot - replaced barplot

--- Figure_5/Fosmidomycin_script.R
+++ Figure_5/Fosmidomycin_script.R
@@ -20,47 +20,19 @@ df.long$level = as.numeric(df.long$level)
 df.long$genotype = factor(df.long$genotype, 
                           levels = c("PI127826", "73", "CV", "411"),
                           ordered = TRUE)
-#Filter data
-df.long %>% filter(.,
-                   df.long$treatment %in% c("control", "fosmidomycin") &
-                   df.long$metabolite == "total_terpenes" &
-                     df.long$genotype == "PI127826" &
-                     df.long$day == "14") %>%
 
-
-#Boxplot
-ggplot(., aes(x=treatment, y=level, fill = phenotype)) +
-  geom_boxplot()+
-  facet_grid(genotype~metabolite, scale = "free") +
-  theme_classic() +
-  theme_linedraw() +
-  theme(text = element_text(family = "Times New Roman", color = "black", size = 8),
-        axis.text.x = element_text(size = 10, angle = 0), 
-        plot.title = element_text(size = rel(2),hjust = 0.5),
-        strip.background = element_rect(colour="black", fill="grey"),
-        strip.text.x = element_text(size=8, colour = "black"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank()) +
-  labs(y = "Metabolite level (ng metabolite / type-VI trichome)") 
-
-  ggsave("Total_cytosolic_Day7.jpg", device = "jpg", scale = 1, width = 28, height = 10, units = "cm", dpi = 300)
-  
-  
 
 #Barplot
 
-sum = summarySE(df.long, 
-                measurevar = "level", 
-                groupvars = c("genotype", "treatment", "day", "metabolite", "phenotype"))
-write_csv(sum, "inhibitor_treatments_summary.csv")
-str(sum)
-
-sum %>% filter(., 
-                   sum$metabolite %in% c("total_MVA_terpenes", "total_MEP_terpenes") & 
-                     sum$day == "14") %>%
-  ggplot(., aes(x=treatment, y=level, fill = phenotype)) +
+df.long %>% 
+  dplyr::group_by(genotype, phenotype, treatment, day, metabolite) %>%
+  dplyr::summarise(mean_level = mean(level), se = sd(level)/sqrt(n())) %>%
+  filter(metabolite %in% c("total_MVA_terpenes", "total_MEP_terpenes") & 
+         day == "14") %>%
+  
+  ggplot(., aes(x=treatment, y=mean_level, fill = phenotype)) +
   geom_bar(stat = "identity")+
-  geom_errorbar(aes(ymin = level- se, ymax = level + se), width=0.1)+
+  geom_errorbar(aes(ymin = mean_level- se, ymax = mean_level + se), width=0.1)+
   facet_grid(genotype~metabolite, scale = "free") +
   theme_bw()
 
