661ee23a828fefa3a5ad54fd2ab686fa55c48c6c
Ruy Kortbeek <r.w.j.kortbeek@uva.nl>
2020-05-20 17:54:46 +0200

    Deseq by ruy

--- Figure_7/Barplot_expression.R
+++ Figure_7/Barplot_expression.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggbiplot)
 
 df = read.delim(file = "Figure_7/abundance_tidy.tsv", header = T)
 
@@ -25,19 +26,39 @@ df.parsed$sample = factor(df.parsed$sample, levels = c("Elite_01", "PI127826_F1"
 
 # load differentially expressed genes
 
-diff <- read.delim(file = "TableS1_DE_genes/differentials.tsv", header = TRUE) %>% 
+diff <- read.delim(file = "Figure_7/DEseq_analysis/F2_RNAseq_DEseq_resuts_significant_genes.tsv", header = TRUE) %>% 
   mutate(target_id = substr(target_id, start = 1, stop = 14)) %>% 
-  arrange(-b)
+  arrange(log2FoldChange)
 
 diff.top10 <- diff[1:10,1]
 
 # Filter by solycnumber and plot
-df.parsed %>% filter(target_id == "Solyc10g083290") %>%
+df.parsed %>% filter(target_id %in% diff.top10) %>%
   ggplot(aes(x = reorder(sample, condition), y = est_counts, fill = condition))+
   geom_bar(stat = "identity")+
   facet_wrap(~target_id, scale = "free")
 
-# Plot per condition (mean active vs. mean lazy)
-df.parsed %>% filter(target_id == "Solyc03g079170") %>%
-  ggplot()+
-  geom_boxplot(aes(x = condition, y = est_counts))
+
+#############################
+# Selection based on counts #
+#############################
+
+df.wide counts = df.parsed  %>% pivot_wider(names_from = sample, values_from = est_counts)
+write.table(df.wide, file = "Figure_7/abundance_wide.tsv", sep = "\t", col.names = TRUE, row.names = FALSE)
+
+df.filtered = df.wide %>% filter(
+                                   
+                                   "F2-28" > "F2-151" &
+                                   "F2-28" > "F2-411") &
+                                   "F2-28" > "F2-455" 
+)
+
+#######
+# PCA #
+#######
+df.for.pca <- df.wide %>% column_to_rownames(var = "target_id")
+df.for.pca <- t(df.for.pca)
+df.for.pca <- log(df.for.pca +1)
+pca <- prcomp(df.for.pca)
+
+ggbiplot(pca, groups = conditions)
---
+++ Figure_7/DEseq_analysis/DEseq_F2.R
@@ -0,0 +1,54 @@
+library(ggplot2)
+library(DESeq2)
+library(pheatmap)
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(plyr)
+library(ggfortify)
+
+# Load reads
+counts = read.table("Figure_7/abundance_tidy.tsv", header = T, sep = "\t") %>% 
+  mutate(target_id = substr(target_id, start = 1, stop = 14)) %>%
+  filter(sample %in% c("Elite_01", "PI127826_F1", "F2-151", "F2-411", "F2-445",
+                       "PI127826", "F2-28", "F2-73", "F2-127"))
+
+# Make data "wide" format
+counts = as.data.frame(pivot_wider(counts, names_from = sample, values_from = est_counts))
+
+# Lazy / Active information on the samples
+sampleinfo <- data.frame(cbind(colnames(counts[,2:10]),
+                               c("lazy","active","lazy","active","lazy","lazy","active","active","lazy")), stringsAsFactors = F)
+
+colnames(sampleinfo) = c("SampleName","Condition")
+
+#shape counts dataset to make it ready for DEseq
+counts4DE <- counts
+counts4DE[2:10] = lapply(counts4DE[2:10], as.integer) # change floating numbers to intergers 
+colnames(counts4DE)[2:10] <- sampleinfo[,1] # Add samples names 
+
+##Clean-up dataset 
+numOverTen <- function(x) {sum(x > 10)} 
+ExpressionNum <- apply(counts4DE[2:10], 1, numOverTen) 
+counts4DE <- counts4DE[which(ExpressionNum > 3),] #throw away data where more less than 3 samples have less than 10 counts 
+counts4DE <- distinct(counts4DE, target_id, .keep_all = TRUE) # Remove rows with duplicated gene names 
+
+
+###################
+# DEseq2 Analysis #
+###################
+
+DES <- DESeqDataSetFromMatrix(counts4DE, sampleinfo, ~ Condition, tidy = TRUE)
+head(DES)
+DES <- DESeq(DES, parallel = T) #creates a normalised dataset
+plotDispEsts(DES)
+plotMA(DES, main = "Lazy and Active Differences in Gene Expression") #Differenitally expressed genes
+
+# Export results
+res = results(DES, contrast = c("Condition","lazy","active"))
+res = as.data.frame(res) %>% rownames_to_column(., var = "target_id")
+res.significant <- res %>% filter(padj < 0.05)
+
+# Export result to txt
+ write.table(res, file = "Figure_7/DEseq_analysis/F2_RNAseq_DEseq_resuts.tsv", sep = "\t", row.names = FALSE)
+ write.table(res.significant, file = "Figure_7/DEseq_analysis/F2_RNAseq_DEseq_resuts_significant_genes.tsv", sep = "\t", row.names = FALSE)
