0c7ae021331707753bf4e4e94fc07918247f087e
Ruy Kortbeek <r.w.j.kortbeek@uva.nl>
2020-08-27 17:12:38 +0200

    Added filter for chromosome number

--- Figure_9/Ruys_heatmap_detected_alleles.R
+++ Figure_9/Ruys_heatmap_detected_alleles.R
@@ -5,17 +5,21 @@ df <- read.delim(file = "Figure_9/DE_genes_detected_alleles.txt",
                  header = TRUE,
                  check.names = FALSE)
 # Make df tidy
-df <- pivot_longer(df, 
+df.long <- pivot_longer(df, 
                    cols = -c(target_id, chromosome),
                    names_to = "genotype",
                    values_to = "allele")
 
 # Remove transcripts of which allele version could no be determined ("NA") 
-df <- na.omit(df)
+df.long.chromosome <- na.omit(df.long) %>% 
+  filter(chromosome == "Chr08") 
 
-df.wide <- df %>% pivot_wider(names_from = genotype, values_from = allele)
+
+
+df.wide <- df.long.chromosome %>% select(-chromosome) %>%
+  pivot_wider(names_from = genotype, values_from = allele)
+
 df.wide = df.wide %>% column_to_rownames(var = "target_id")
-
 pheatmap(df.wide,
          cluster_rows = FALSE,
          cluster_cols = FALSE)
