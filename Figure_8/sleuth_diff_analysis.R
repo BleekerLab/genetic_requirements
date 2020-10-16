10ce86b466d529f987a6795f5c62acf566606f22
Marc Galland <m.galland@uva.nl>
2020-10-16 12:30:00 +0200

    update script

--- Figure_8/sleuth_diff_analysis.R
+++ Figure_8/sleuth_diff_analysis.R
@@ -7,7 +7,7 @@ library("sleuth")  #devtools::install_github("pachterlab/sleuth", dependencies =
 kallistos <- list.dirs("results/kallisto/",full.names = TRUE, recursive = FALSE)
 
 
-samples2condition <- read.delim(file = "config/samples.tsv", 
+samples2condition <- read.delim(file = "samples.tsv", 
                                header = TRUE, 
                                stringsAsFactors = FALSE) %>% 
   dplyr::select("sample", "condition") %>% 
@@ -16,16 +16,20 @@ samples2condition <- read.delim(file = "config/samples.tsv",
 
 so <- sleuth_prep(samples2condition, full_model = ~ condition, extra_bootstrap_summary = TRUE)
 
-###########
-# Fit model
-###########
+###############################
+# Fit model + perform Wald test
+###############################
 
 so <- sleuth_fit(obj = so, formula = ~ condition, fit_name = 'full')
 so <- sleuth_wt(so, which_beta = "conditionactive", which_model = "full")
-sleuth::plot_volcano(obj = so, test = "conditionactive", test_type = "wt",which_model = "full",sig_level = 0.01)
 
+all_genes <- sleuth_results(so, test = "conditionactive", test_type = "wt",show_all = TRUE) 
+significant_genes <- filter(all_genes, qval <= 0.05)
+  
 #########################################
 # Sleuth live for interactive exploration
 #########################################
+
+save(so, file = "kallisto_sleuth_analysis.RData")
 
 sleuth_live(so)
