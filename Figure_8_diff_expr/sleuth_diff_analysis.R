3c6f2fcf241a26eb90d0b02ef1741abd20fa335b
Marc Galland <m.galland@uva.nl>
2021-03-09 11:01:10 +0100

    update RNA-seq analysis with Kallisto and the new F2-28 plant

--- Figure_8_diff_expr/sleuth_diff_analysis.R
+++ Figure_8_diff_expr/sleuth_diff_analysis.R
@@ -1,35 +1,90 @@
+library("checkpoint")
+checkpoint("2020-01-01")
 library("tidyverse")
-library("sleuth")  #devtools::install_github("pachterlab/sleuth", dependencies = TRUE, upgrade = TRUE, quiet = TRUE)
 
+# v0.29.0 (because 0.30.0 has bugs)
+# devtools::install_github(repo = "pachterlab/sleuth",
+#                         ref = "2cbb28749875eaf7dcafb30f9a795313c481fcb1", 
+#                           dependencies = TRUE, 
+#                           upgrade = TRUE, 
+#                           quiet = TRUE,
+#                           force = TRUE)
+library("sleuth")  
 
+###############################
 # Creation of the Sleuth object
+###############################
+kallistos <- list.dirs("Supplemental_data_RNA-seq/kallisto",
+                       full.names = TRUE, 
+                       recursive = FALSE) 
+kallistos_df <- data.frame(path = kallistos, 
+                           sample = basename(kallistos)) 
 
-kallistos <- list.dirs("results/kallisto/",full.names = TRUE, recursive = FALSE)
-
-
-samples2condition <- read.delim(file = "samples.tsv", 
+samples2condition <- read.delim(file = "Supplemental_data_RNA-seq/config/samples.tsv", 
                                header = TRUE, 
                                stringsAsFactors = FALSE) %>% 
   dplyr::select("sample", "condition") %>% 
-  dplyr::mutate(path = kallistos)
-
-
-so <- sleuth_prep(samples2condition, full_model = ~ condition, extra_bootstrap_summary = TRUE)
+  inner_join(x = ., y = kallistos_df, by = "sample") %>% 
+  mutate(path = as.character(path))
+  
+so <- sleuth_prep(samples2condition, 
+                  full_model = ~ condition, 
+                  extra_bootstrap_summary = TRUE)
 
 ###############################
 # Fit model + perform Wald test
 ###############################
 
-so <- sleuth_fit(obj = so, formula = ~ condition, fit_name = 'full')
-so <- sleuth_wt(so, which_beta = "conditionactive", which_model = "full")
+so <- sleuth_fit(obj = so, 
+                 formula = ~ condition, 
+                 fit_name = 'full')
 
-all_genes <- sleuth_results(so, test = "conditionactive", test_type = "wt",show_all = TRUE) 
-significant_genes <- filter(all_genes, qval <= 0.05)
-  
+so <- sleuth_wt(so, 
+                which_beta = "conditionactive", 
+                which_model = "full")
+
+all_genes_wald <- sleuth_results(so, 
+                            test = "conditionactive", 
+                            test_type = "wt",
+                            show_all = TRUE) 
+
+significant_genes <- filter(all_genes, 
+                            qval <= 0.05) %>% 
+  rename("gene" = "target_id")
+
+#####################################
+# Fit model + perform likelihood test
+#####################################
+
+so <- sleuth_fit(so, ~ condition, fit_name = "full")
+so <- sleuth_fit(so, ~ 1, fit_name = "reduced")
+so <- sleuth_lrt(so, null_model = "reduced", alt_model = "full")
+
+all_genes_lrt <- sleuth_results(so, 
+                            test = "reduced:full", 
+                            test_type = "lrt",
+                            show_all = TRUE) 
+
+significant_genes_lrt <- filter(all_genes_lrt, 
+                            qval <= 0.05) %>% 
+  rename("gene" = "target_id")
+
+##############################################
+# Annotate significant genes and save R object
+##############################################
+
+gene_annotations <- read.csv(file = "Figure_8_diff_expr/ITAG4.1_descriptions.csv", 
+                              stringsAsFactors = FALSE, 
+                              header = TRUE)
+
+significant_genes_annotated <- inner_join(significant_genes_wald, 
+                                          gene_annotations, 
+                                          by = "gene")
+
+
+save(so, file = "Figure_8_diff_expr/kallisto_sleuth_analysis.RData")
+
 #########################################
 # Sleuth live for interactive exploration
 #########################################
-
-save(so, file = "kallisto_sleuth_analysis.RData")
-
 sleuth_live(so)
--- Supplemental_data_RNA-seq/README.md
+++ Supplemental_data_RNA-seq/README.md
@@ -5,11 +5,8 @@ This folder contains the `raw_counts.tsv` and the `scaled_counts.tsv` that are u
 <!-- MarkdownTOC autolink="true" levels="1,2" -->
 
 - [Reports from the analysis](#reports-from-the-analysis)
-	- [FastQC metrics](#fastqc-metrics)
+	- [MultiQC report from the individual fastp reports](#multiqc-report-from-the-individual-fastp-reports)
 	- [Mapping report](#mapping-report)
-- [Raw and scaled counts comparisons](#raw-and-scaled-counts-comparisons)
-	- [Boxplots](#boxplots)
-	- [Scatterplots](#scatterplots)
 - [Code and data used](#code-and-data-used)
 	- [Sequencing files](#sequencing-files)
 	- [Software](#software)
@@ -20,33 +17,41 @@ This folder contains the `raw_counts.tsv` and the `scaled_counts.tsv` that are u
 
 # Reports from the analysis
 
-## FastQC metrics
+## MultiQC report from the individual fastp reports
+
+Open the [MultiQC report](./multiqc_report.html) in a web browser. 
 
 ## Mapping report
 
-See [mapping summary table](./mapping_summary.tsv)
+See [mapping summary table](./mapping_results.csv)
 
-# Raw and scaled counts comparisons
-To determine the effect of DESeq2 scaling procedure, two plots were made using the [`raw_versus_scaled_counts.R`](./raw_versus_scaled_counts.R) script.  
-Results are plotted on a log10 axis (with an offset of 1 to avoid -Inf for gene counts equal to 0).
-
-## Boxplots
-
-<img src="raw_vs_scaled_boxplot_counts.png" width="60%">
-
-## Scatterplots
-
-<img src="scatterplots_raw_vs_scaled.png" width="60%">
+| n_targets 	| n_bootstraps 	| n_processed 	| n_pseudoaligned 	| n_unique 	| p_pseudoaligned 	| p_unique 	| sample      	|
+|-----------	|--------------	|-------------	|-----------------	|----------	|-----------------	|----------	|-------------	|
+| 34075     	| 100          	| 47839301    	| 43861023        	| 43085770 	| 91.7            	| 90.1     	| Elite_02    	|
+| 34075     	| 100          	| 25763854    	| 22315511        	| 22086331 	| 86.6            	| 85.7     	| F2-127      	|
+| 34075     	| 100          	| 23387126    	| 20474496        	| 19872926 	| 87.5            	| 85       	| F2-151      	|
+| 34075     	| 100          	| 83670152    	| 57901546        	| 55706238 	| 69.2            	| 66.6     	| F2-28       	|
+| 34075     	| 100          	| 29535331    	| 25972873        	| 25559982 	| 87.9            	| 86.5     	| F2-411      	|
+| 34075     	| 100          	| 27980463    	| 24514301        	| 24165926 	| 87.6            	| 86.4     	| F2-445      	|
+| 34075     	| 100          	| 28190078    	| 24914196        	| 24645285 	| 88.4            	| 87.4     	| F2-73       	|
+| 34075     	| 100          	| 48627207    	| 41552140        	| 40707092 	| 85.5            	| 83.7     	| PI127826    	|
+| 34075     	| 100          	| 32277727    	| 28364133        	| 27987404 	| 87.9            	| 86.7     	| PI127826_F1 	|
 
 # Code and data used
 
 ## Sequencing files
 - F1, Elite line and S. habrochaites PI127826 mRNA-seq fastq files from stem trichomes: [link](https://doi.org/10.5281/zenodo.3603229) 
-- F2 lines mRNA-seq fastq files from stem trichomes: [link](https://doi.org/10.5281/zenodo.3610278)
+- F2 lines mRNA-seq fastq files from stem trichomes: [link](https://doi.org/10.5281/zenodo.4491747)
+
+The F2-28 replicates 1 to 4 were pooled together to constitute one unique F2-28 individual plant.  
+The md5 checksum of the pooled F2-28 sample is `74032ef1726a9013bf4b53f7475fbc2f`. 
 
 ## Software 
-Snakemake mRNA-seq pipeline: [link](https://zenodo.org/record/4034215)
+Snakemake mRNA-seq pipeline based on Kallisto: [link](https://zenodo.org/record/4091104)
+
 
 ## Genomic references
-Tomato assembly and annotation SL4.0
+The tomato annotation ITAG4.0 was used. Specifally the cDNA fasta file was used.   
 
+[SOL Genomics link](ftp://ftp.solgenomics.net/tomato_genome/annotation/ITAG4.0_release/ITAG4.0_cDNA.fasta)  
+[Custom Zenodo record](https://10.5281/zenodo.4321000)
