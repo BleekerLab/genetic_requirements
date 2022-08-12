d668e16426f409100b2b3fbf0aac90e79bb3b9ea
Marc Galland <m.galland@uva.nl>
2022-08-12 14:48:05 +0200

    diff expr

--- Figure_08_diff_expr/README.md
+++ Figure_08_diff_expr/README.md
@@ -2,3 +2,48 @@
 
 The description of the fastq mRNA-seq to Kallisto pseudoalignments is described in the [`Supplemental_data_RNA-seq/` folder.](../Supplemental_data_RNA-seq/)  
 
+# Chromomaps
+
+The [chromoMap](https://cran.r-project.org/web/packages/chromoMap/index.html) package depends on R >4.0. Since I still use R 3.6 for compatibility reasons, I will use a Docker image to create the chromomaps. 
+
+## RStudio 4.1 docker image
+To create the chromomaps figure, first get the RStudio docker image for version 4.1: `docker pull rocker/rstudio:4.1`.
+
+Inside `Figure_08_diff_expr/`, do:  
+```bash
+docker run --detach -p 8787:8787 -e DISABLE_AUTH=true -v $PWD:/home/rstudio/workspace/ rocker/rstudio:4.1
+```
+Then go to [http://localhost:8787/](http://localhost:8787/). 
+
+## Install chromoMap
+
+1. `install.packages("chromoMap")`
+2. 
+
+## Chromosome sizes
+
+These were obtained using samtools:  
+```bash
+samtools faidx S_lycopersicum_chromosomes.2.50.fa
+cut -f1,2 S_lycopersicum_chromosomes.2.50.fa.fai > sizes.genome
+```
+Followed by:
+```bash
+awk '{print $1 "\t" 1 "\t" $2}' sizes.genome 
+```
+This gives:  
+```
+SL2.50ch00	1	21805821
+SL2.50ch01	1	98543444
+SL2.50ch02	1	55340444
+SL2.50ch03	1	70787664
+SL2.50ch04	1	66470942
+SL2.50ch05	1	65875088
+SL2.50ch06	1	49751636
+SL2.50ch07	1	68045021
+SL2.50ch08	1	65866657
+SL2.50ch09	1	72482091
+SL2.50ch10	1	65527505
+SL2.50ch11	1	56302525
+SL2.50ch12	1	67145203
+```
--- Figure_10_SNPs_from_RNAseq/bam_to_snps_to_bed.sh
+++ Figure_10_SNPs_from_RNAseq/bam_to_snps_to_bed.sh
@@ -7,6 +7,7 @@
 # following this biostar post: https://www.biostars.org/p/327558/
 #################################################################
 
+### Plant F2-28
 bcftools mpileup --threads 30 --max-depth 10000 --redo-BAQ --min-BQ 30 --per-sample-mF -f S_lycopersicum_chromosomes.2.50.fa \
 --skip-indels --output-type v ../results_lyco_genome/star/F2-28_concat_Aligned.sortedByCoord.out.bam | \
 bcftools call --multiallelic-caller --variants-only -Ov - > F2-28_concat.vcf 
@@ -15,6 +16,21 @@ bcftools call --multiallelic-caller --variants-only -Ov - > F2-28_concat.vcf
 vcfutils.pl varFilter -d 10 F2-28_concat.vcf  > F2-28_concat.filtered.vcf
 
 vcf2bed < F2-28_concat.filtered.vcf > F2-28_concat.filtered.bed
+
+
+##########################################
+# Intersect
+# Keep SNPs common to PI127826 and F2-28
+# Remove SNPs from lycopersicum Elite line (-v option)
+##########################################
+
+bcftools isec -p isec_output -Oz F2-28/F2-28_concat.filtered.vcf.gz PI127826/PI127826.filtered.vcf.gz 
+
+# Common SNPs are in isec_output/0002.vcf.gz
+# isec_output/0002.vcf.gz	for records from F2-28/F2-28_concat.filtered.vcf.gz shared by both	F2-28/F2-28_concat.filtered.vcf.gz PI127826/PI127826.filtered.vcf.gz
+
+# rename 0002.vcf.gz to F2-28_intersect_PI127826.vcf.gz
+vcf2bed < F2-28_intersect_PI127826.vcf > F2-28_intersect_PI127826.bed
 
 ##############################################
 # Make windows from the S. lycopersicum genome 
@@ -31,6 +47,6 @@ bedtools makewindows -g chromsizes.txt -w $WINDOW_SIZE > genome.bed
 # Count number of SNPs per 1Mb window
 ######################################
 
-bedmap --echo --count --delim "\t" genome.bed F2-28_concat.filtered.bed> F2-28_snp_counts.tsv 
+bedmap --echo --count --delim "\t" genome.bed F2-28_concat.filtered.bed > F2-28_snp_counts.tsv 
 
 
